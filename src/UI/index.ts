bbcdd19d5c7fab2f269e4e7cf97571d65102ff73
vuongle2609 <vuongle2609@gmail.com>
2023-12-05 15:44:51 +0700

    done format

--- src/UI/index.ts
+++ src/UI/index.ts
@@ -1,29 +1,11 @@
-import { createActor, createMachine } from "xstate";
 import MainScreen from "./mainScreen";
 
 export default class UI {
   mainScreen = new MainScreen();
 
-  uiMachine = createMachine({
-    context: {
-      count: 0,
-    },
-    on: {
-      HOME: {
-        actions: () => {
-          this.mainScreen.render();
-        },
-      },
-    },
-  });
-
-  uiActor = createActor(this.uiMachine).start();
-
   constructor() {
     this.initialize();
   }
 
-  initialize() {
-    this.uiActor.send({ type: "HOME" });
-  }
+  initialize() {}
 }
